c76bf710b1dd5e08e9c971baecab5d2f666fce2b
小六 <1203258449@qq.com>
2019-03-04 19:31:27 +0800

    数组：求最大最小值和平均值

---
+++ begin/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    public static void main(String[] args) {
+		int[] data = new int[]{1,4,3,4,55,77,6,9,8} ; int max = data[0] ; // 假定第一个元素为最大值
+		int min = data[0] ; // 假定第一个元素为最小值
+		int sum = data[0] ;
+       for (int i = 1 ; i< data.length; i++){
+           sum += data[i] ;
+           if (data[i]>max){
+               max = data[i] ;
+           }
+           if (data[i]<min){
+               min = data[i] ;
+		   }
+	   }
+	   System.out.println("最大值为:" +max);
+	   System.out.println("最小值为:"+min); 
+	   System.out.println("总和为:"+sum);
+	   System.out.println("平均值为:"+(double)sum/data.length);
+	}
+}
+	   
--- begin/乘法口诀表.java
+++ begin/乘法口诀表.java
@@ -1,6 +1,7 @@
 public class 乘法口诀表{
 	public static void main(String[] args){
-		for(int i=1;i<=9;i++){
+		int n=Integer.parselnt(args[0]);
+		for(int i=1;i<=n;i++){
 			for(int j=1;j<=i;j++){
 				System.out.print(i+"*"+j+"="+i*j+"\t");
 			}
